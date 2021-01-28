3c37113d73ec303b02ca034adeddf8d4d4027f78
Nicolas Velasquez <velasquez@iloguest.org>
2021-01-28 22:39:39 +0100

    Compact stats and start message

--- app/src/main.py
+++ app/src/main.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
     '''Main script/function of the whole project.'''
-    logging.info("Starting: %s -> %s", config.start, config.end)
+    logging.info("Filter start time : %s, filter end time -> %s", config.start, config.end)
     msgraph_connector = MSGraphConnector()
     misp_attributes = get_misp_attributes()
     combined_misp_msgraph_dicts = list(map(lambda misp_attr: transform_misp_to_msgraph(misp_attr, config), misp_attributes))
@@ -23,25 +23,24 @@ def main():
         'UNKNOWN': 0,
         'CORRUPT': 0
     }
-    push_counter = 0
+    push_to_msgraph = 0
     for combined_misp_msgraph_dict in combined_misp_msgraph_dicts:
         transform_counters[combined_misp_msgraph_dict['transform_status']] += 1
         if combined_misp_msgraph_dict['transform_status'] == "SUCCESS":
             msgraph_connector.post_one_ioc_to_graph(combined_misp_msgraph_dict)
-            push_counter += 1 if combined_misp_msgraph_dict['post_status'] == "SUCCESS" else 0
+            push_to_msgraph += 1 if combined_misp_msgraph_dict['post_status'] == "SUCCESS" else 0
 
-    num_total = len(combined_misp_msgraph_dicts)
-    logging.info('Total number of MISP attributes pulled: %s.', num_total)
-    if num_total > 0:
-        __print_stats("Num of MISP attrs correctly transformed to MS Graph: ", transform_counters['SUCCESS'], num_total)
-        __print_stats("Num of MISP attrs ignored: ", transform_counters['IGNORED'], num_total)
-        __print_stats("Num of MISP attrs ignored (Defender): ", transform_counters['IGNORE DEFENDER'], num_total)
-        __print_stats("Num of MISP attrs unknown/corrupt: ", transform_counters['UNKNOWN'], num_total)
-        __print_stats("Num of MISP attrs pushed to MS Graph: ", push_counter, num_total)
+    misp_attrs = len(combined_misp_msgraph_dicts)
+    logging.info( \
+        'MISP attrs: %s -> into MSGraph IOC: %s -> pushed to MSGraph: %s. Ignored: %s, ignored defender: %s, corrupt/unknown: %s.',
+        misp_attrs,
+        transform_counters['SUCCESS'],
+        push_to_msgraph,
+        transform_counters['IGNORED'],
+        transform_counters['IGNORE DEFENDER'],
+        transform_counters['UNKNOWN']
+    )
     logging.info("Finished")
-
-def __print_stats(text, count, total):
-    logging.info('%s: %s. %.2f%%', text, count, 100*count/total)
 
 if __name__ == "__main__":
     main()
