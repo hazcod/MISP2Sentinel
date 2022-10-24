bb10a8980c4bd6b558513d02839d19f12bafa4d5
Nicolas Velasquez <gnicolax@gmail.com>
2022-10-24 09:06:47 +0200

    Update config for new names

--- app/src/config.py
+++ app/src/config.py
@@ -1,25 +1,31 @@
 """Config variables script."""
 from os import environ
 
+RECENT_NUM_DAYS = 5
+
 # MISP
 MISP_BASE_URL = environ.get("MISP_BASE_URL")
 MISP_KEY = environ.get("MISP_KEY")
 MISP_CA_BUNDLE = environ.get("MISP_CA_BUNDLE")
 MISP_EVENT_FILTERS = {
-    "timestamp": "5h",
+    "timestamp": f"{RECENT_NUM_DAYS}d",
     "published": True,
+    # "limit": 1,
 }
 MISP_TIMEOUT = 120
 
-# GRAPH
-GRAPH_AUTH = {
-    "tenant": environ.get("MSGRAPH_TENANT_ID"),
-    "client_id": environ.get("MSGRAPH_CLIENT_ID"),
-    "client_secret": environ.get("MSGRAPH_CLIENT_SECRET"),
-}
-GRAPH_TARGET_PRODUCT = environ.get("MSGRAPH_TARGET_PRODUCT")
-GRAPH_ACTION = "alert"
-GRAPH_PASSIVE_ONLY = False
-GRAPH_DAYS_TO_EXPIRE = (
+# AZURE (MSGRAPH/MANAGEMENT)
+AZ_AUTH_TENANT_ID = environ.get("MSGRAPH_TENANT_ID")
+AZ_AUTH_CLIENT_ID = environ.get("MSGRAPH_CLIENT_ID")
+AZ_AUTH_CLIENT_SECRET = environ.get("MSGRAPH_CLIENT_SECRET")
+
+AZ_SUBSCRIPTION = environ.get("AZ_SUBSCRIPTION")
+AZ_SENTINEL_RG = environ.get("AZ_SENTINEL_RG")
+AZ_SENTINEL_WORKSPACE_NAME = environ.get("AZ_SENTINEL_WORKSPACE_NAME")
+
+AZ_TARGET_PRODUCT = environ.get("MSGRAPH_TARGET_PRODUCT")
+AZ_ACTION = "alert"
+AZ_PASSIVE_ONLY = False
+AZ_DAYS_TO_EXPIRE = (
     int(environ.get("MSGRAPH_DAYS_TO_EXPIRE")) if "MSGRAPH_DAYS_TO_EXPIRE" in environ else None
 )
