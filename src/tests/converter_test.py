ae4f5c8fa5d52b78259b53e725a5abc14cbaa365
Nicolas Velasquez <gnicolax@gmail.com>
2022-10-25 20:31:32 +0200

    Add test for domain-name

--- src/tests/converter_test.py
+++ src/tests/converter_test.py
@@ -45,3 +45,41 @@ def test_converter_url():
     )
 
     assert misp_iocs_as_sentinel[0] == expected_sentinel_ioc
+
+
+def test_converter_domain():
+    misp_ioc = json.loads(
+        """
+            {
+                "id": "3931551",
+                "event_id": "8608",
+                "category": "Network activity",
+                "type": "domain",
+                "uuid": "d3f50f40-3b30-4909-94c0-f2e7370c71be",
+                "timestamp": "1666161611",
+                "value": "bharatbhushanaward.net",
+                "Event": {
+                    "info": "Online File Converter Phishing Page Spreads RedLine Stealer"
+                }
+            }
+        """
+    )
+    misp_iocs_as_sentinel = converter.transform_iocs_misp_to_sentinel([misp_ioc], 2, "misp_label")
+
+    expected_sentinel_ioc = converter.SentinelIOC(
+        description=(
+            "(misp_label event_id: 8608) Online File "
+            "Converter Phishing Page Spreads RedLine Stealer"
+        ),
+        displayName="misp_label_attribute_3931551",
+        externalId="d3f50f40-3b30-4909-94c0-f2e7370c71be",
+        pattern="[domain-name:value = 'bharatbhushanaward.net']",
+        patternType="stix",
+        source="misp_label",
+        threatIntelligenceTags=["misp_label_event_id_8608", "misp_label_attribute_id_3931551"],
+        threatTypes=["Network activity"],
+        validFrom="2022-10-19T06:40:11+00:00",
+        validUntil="2022-10-21T06:40:11+00:00",
+    )
+
+    assert misp_iocs_as_sentinel[0] == expected_sentinel_ioc
