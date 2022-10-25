f47c000d54221f0ec99e8355d572afff191fe4bb
Nicolas Velasquez <gnicolax@gmail.com>
2022-10-25 20:31:20 +0200

    Make lines width less than 125 chars

--- src/tests/converter_test.py
+++ src/tests/converter_test.py
@@ -12,24 +12,27 @@ from misp_to_sentinel import converter
 def test_converter_url():
     misp_ioc = json.loads(
         """
-{
-    "id": "3931551",
-    "event_id": "8608",
-    "category": "Network activity",
-    "type": "url",
-    "uuid": "d3f50f40-3b30-4909-94c0-f2e7370c71be",
-    "timestamp": "1666161611",
-    "value": "https://convertigoto.net/cc/flesd/YourConvertedFile59417.exe",
-    "Event": {
-        "info": "Online File Converter Phishing Page Spreads RedLine Stealer"
-    }
-  }
-  """
+            {
+                "id": "3931551",
+                "event_id": "8608",
+                "category": "Network activity",
+                "type": "url",
+                "uuid": "d3f50f40-3b30-4909-94c0-f2e7370c71be",
+                "timestamp": "1666161611",
+                "value": "https://convertigoto.net/cc/flesd/YourConvertedFile59417.exe",
+                "Event": {
+                    "info": "Online File Converter Phishing Page Spreads RedLine Stealer"
+                }
+            }
+        """
     )
     misp_iocs_as_sentinel = converter.transform_iocs_misp_to_sentinel([misp_ioc], 2, "misp_label")
 
     expected_sentinel_ioc = converter.SentinelIOC(
-        description="(misp_label event_id: 8608) Online File Converter Phishing Page Spreads RedLine Stealer",
+        description=(
+            "(misp_label event_id: 8608) Online File "
+            "Converter Phishing Page Spreads RedLine Stealer"
+        ),
         displayName="misp_label_attribute_3931551",
         externalId="d3f50f40-3b30-4909-94c0-f2e7370c71be",
         pattern="[url:value = 'https://convertigoto.net/cc/flesd/YourConvertedFile59417.exe']",
